7d744e22cd484e32484e35c98f001003fb4b2626
ᴡᴏᴊᴄɪᴇᴄʜ ɢʀᴢʏʙᴏᴡꜱᴋɪ <wojgrz5@st.amu.edu.pl>
2023-05-20 18:57:53 +0200

    Reorganised files into directories

--- sort.js
+++
@@ -1,8 +0,0 @@
-function checkClick(id) {
-    document.querySelector('input[name="nrtabeli"]').value = "DESC";
-    document.querySelector('input[name="data"]').value = "DESC";
-    document.querySelector('input[name="kurs"]').value = "DESC";
-    document.querySelector('input[name="'+id+'"').value = "ASC";
-
-    // document.getElementById('form').submit()
-}
